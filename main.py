4feebba53e8e5a88490c639364f30f3ccecc4ae8
Vic Wen <vic20050418@gmail.com>
2023-11-28 09:48:49 +0800

    v0.0.2

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import os
 from crawler import get
 
 bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-user_id = os.environ.get('USER_ID')
+message_text = get()
+processed_ids = set()
 
 async def main():
     bot = telegram.Bot(bot_token)
     async with bot:
-       await bot.send_message(text=get(), chat_id=user_id)
+        updates = await bot.get_updates()
+
+        for update in updates:
+            user = update.message.chat
+            user_id = user.id
+            if user_id not in processed_ids:
+                processed_ids.add(user_id)
+                await bot.send_message(text=message_text, chat_id=user_id)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
